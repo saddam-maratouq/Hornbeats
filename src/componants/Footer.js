c33d6b30887242874887e08e054aa23f579362a7
saddam-maratouq <sadam.sadam.sadam@hotmail.com>
2021-08-08 23:08:15 +0300

    back tommowrow

---
+++ src/componants/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+
+
+
+
+
+
+
+class footer extends React.Component{
+  render() {
+  return(
+
+    
+   <div>
+         &copy; Saddam maratouq    
+         </div>
+  )
+  }
+}
+export default footer ; 
